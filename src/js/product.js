10d01dd2bf8fc2f736d99aab12dafbc29d2fd91e
garyselios <garyseliosbennett@hotmail.com>
2026-03-12 19:26:02 -0300

    Add missing product detail files from team branch gs--team2

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,9 @@
-import ProductData from "./ProductData.mjs";
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  // Get current cart contents
-  let cartContents = getLocalStorage("so-cart");
-
-  // Ensure cart is an array
-  let cart = Array.isArray(cartContents) ? cartContents : [];
-
-  // Add new product to the cart array
-  cart.push(product);
-
-  // Save the entire cart array back to localStorage
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
